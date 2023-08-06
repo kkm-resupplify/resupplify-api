ee557ef188e9b75b59d6617c72706408ee36f54b
potrec <potrec123@wp.pl>
2023-08-06 21:07:32 +0200

    Added Company model

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -6,23 +6,41 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Company extends Model
 {
     use HasFactory;
 
-    public function user(): BelongsTo
-    {
-        return $this->belongsTo(User::class);
-    }
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+    ];
 
     public function companyCategories(): HasMany
     {
         return $this->hasMany(CompanyCategory::class);
     }
 
+    public function companySubcategories(): HasManyThrough
+    {
+        return $this->hasManyThrough(CompanySubcategory::class, CompanyCategory::class);
+    }
+
     public function companyProducts(): HasMany
     {
         return $this->hasMany(CompanyProduct::class);
+    }
+
+    public function companyMembers(): HasMany
+    {
+        return $this->hasMany(CompanyMember::class);
+    }
+
+    public function owner(): HasOne
+    {
+        return $this->hasOne(User::class);
     }
 }
