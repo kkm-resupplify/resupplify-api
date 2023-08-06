715756630ea2c0291f498a4fade10d076150a3e5
potrec <potrec123@wp.pl>
2023-08-06 19:09:31 +0200

    Added company categories

--- app/Models/CompanySubcategory.php
+++ app/Models/CompanySubcategory.php
@@ -4,13 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class CompanySubcategory extends Model
 {
     use HasFactory;
-    public function companyProduct(): HasMany
+    public function companyProduct()
     {
         return $this -> hasMany(CompanyProduct::class);
+    }
+    public function companyCategory()
+    {
+        return $this -> belongsTo(CompanyCategory::class);
     }
 }
