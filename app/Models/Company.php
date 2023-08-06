c3ffcb7b2ff12f4e2a6fd044912e3dcaf55919c4
kamil_pula <sorennx@gmail.com>
2023-08-06 12:42:16 +0200

    Create Company.php

---
+++ app/Models/Company.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Company extends Model
+{
+    use HasFactory;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function companyCategories(): HasMany
+    {
+        return $this->hasMany(CompanyCategory::class);
+    }
+
+    public function companyProducts(): HasMany
+    {
+        return $this->hasMany(CompanyProduct::class);
+    }
+}
