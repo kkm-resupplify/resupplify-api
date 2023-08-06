1e32ad011f802fa8c7ff267bb4b7983b664e7ad6
kamil_pula <sorennx@gmail.com>
2023-08-06 19:05:02 +0200

    Create CompanyMember.php

---
+++ app/Models/CompanyMember.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class CompanyMember extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'company_role'];
+
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function user(): HasOne
+    {
+        return $this->hasOne(User::class);
+    }
+}
