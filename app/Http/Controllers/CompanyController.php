1cc4cf654273c09f6e07003317de8b3e732f9ffc
potrec <potrec123@wp.pl>
2023-09-27 02:06:41 +0200

    Code refactorization

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Auth;
 
 class CompanyController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
     public static function slugify($text, string $divider = '-')
     {
         // replace non letter or digits by divider
@@ -45,6 +51,13 @@ class CompanyController extends Controller
     public function index()
     {
         //
+        $user = Auth::user();
+        return response()->json(
+            [
+                'message' => 'Company info',
+                'data' => $user->companyMember->company
+            ]
+        );
     }
 
     /**
@@ -53,6 +66,11 @@ class CompanyController extends Controller
     public function create()
     {
         //
+        return response()->json(
+            [
+                'message' => 'create'
+            ]
+        );
     }
 
     /**
@@ -60,47 +78,32 @@ class CompanyController extends Controller
      */
     public function store(StoreCompanyRequest $request)
     {
+        $user = Auth::user();
+        if ($user->companyMember) {
+            return response()->json(
+                [
+                    'message' => 'You can only create one company',
+                    'data' => $user->companyMember
+                ]
+            );
+        }
+
         try {
-            $user = Auth::user();
-            if($user->companyMember)
-            {
-                return response()->json(
-                    [
-                        'message' => 'You can only create one company',
-                        'data' => $user->companyMember
-                    ]
-                    );
-            }
-            $company = Company::create([
-                'name' => $request->input('name'),
-                'slug' => self::slugify($request->input('name')),
-                'description' => $request->input('description'),
-                'verificationStatus' => 'pending',
-                'user_id' => $user->id,
-            ]);
-            $companyAbout = CompanyDetails::create([
-                'company_id' => $company->id,
-            ]);
-            $companyRoles = CompanyRole::create([
-                'name' => 'admin',
-                'permission_level' => 1,
-                'company_id' => $company->id,
-            ]);
-            $companyMember = CompanyMember::create([
-                'user_id' => $user->id,
-                'company_id' => $company->id,
-                'company_role_id' => $companyRoles->id
-            ]);
+            $company = $this->createCompany($request, $user);
+            $companyAbout = $this->createCompanyDetails($company->id);
+            $companyRoles = $this->createCompanyRoles($company->id);
+            $companyMember = $this->createCompanyMember($user->id, $company->id, $companyRoles->id);
+
             return response()->json([
                 'message' => 'Company created successfully',
-                'data' => 
+                'data' =>
                 [
                     'company' => $company,
                     'company_about' => $companyAbout,
                     'company_roles' => $companyRoles,
                     'company_member' => $companyMember
                 ]
-                ]);
+            ]);
         } catch (\Exception $e) {
             return response()->json(
                 [
@@ -113,11 +116,135 @@ class CompanyController extends Controller
     }
 
     /**
+     * Create a new company
+     */
+    private function createCompany($request, $user)
+    {
+        return Company::create([
+            'name' => $request->input('name'),
+            'slug' => self::slugify($request->input('name')),
+            'description' => $request->input('description'),
+            'verificationStatus' => 'pending',
+            'user_id' => $user->id,
+        ]);
+    }
+
+    /**
+     * Create company basic details
+     * @param int $companyId
+     * @return CompanyDetails
+     */
+    private function createCompanyDetails($companyId)
+    {
+        return CompanyDetails::create([
+            'company_id' => $companyId,
+        ]);
+    }
+
+    /**
+     * Create company basic roles
+     * @param int $companyId
+     * @return CompanyRole
+     */
+    private function createCompanyRoles($companyId)
+    {
+        $ownerRole = CompanyRole::create([
+            'name' => 'owner',
+            'permission_level' => 1,
+            'company_id' => $companyId,
+        ]);
+
+        CompanyRole::create([
+            'name' => 'admin',
+            'permission_level' => 1,
+            'company_id' => $companyId,
+        ]);
+
+        CompanyRole::create([
+            'name' => 'worker',
+            'permission_level' => 1,
+            'company_id' => $companyId,
+        ]);
+
+        return $ownerRole;
+    }
+
+    /**
+     * Assign owner to the company 
+     * @param int $userId
+     * @param int $companyId
+     * @param int $roleId
+     * @return CompanyMember
+     */
+    private function createCompanyMember($userId, $companyId, $roleId)
+    {
+        return CompanyMember::create([
+            'user_id' => $userId,
+            'company_id' => $companyId,
+            'company_role_id' => $roleId
+        ]);
+    }
+
+    /**
+     * Add user to the company
+     */
+    public function addUserToCompany()
+    {
+    }
+
+    /**
+     * Return roles of the company
+     */
+    public function getCompanyRoles()
+    {
+    }
+
+    /**
+     * Delete user from the company
+     */
+    public function deleteUserFromCompany()
+    {
+    }
+
+    /**
+     * Delete role of the company
+     */
+    public function deleteRoleFromCompany()
+    {
+    }
+
+    /**
+     * Edit role of the company
+     */
+    public function editRoleFromCompany()
+    {
+    }
+
+    /**
+     * Edit company information
+     */
+    public function editCompanyInformation()
+    {
+    }
+
+    /**
+     * Delete company
+     */
+    public function deleteCompany()
+    {
+    }
+
+    /**
      * Display the specified resource.
      */
     public function show(Company $company)
     {
         //
+        return response()->json(
+            [
+                'message' => 'show'
+            ]
+        );
     }
 
     /**
@@ -126,6 +253,11 @@ class CompanyController extends Controller
     public function edit(Company $company)
     {
         //
+        return response()->json(
+            [
+                'message' => 'edit'
+            ]
+        );
     }
 
     /**
@@ -134,6 +266,11 @@ class CompanyController extends Controller
     public function update(UpdateCompanyRequest $request, Company $company)
     {
         //
+        return response()->json(
+            [
+                'message' => 'update'
+            ]
+        );
     }
 
     /**
@@ -142,5 +279,10 @@ class CompanyController extends Controller
     public function destroy(Company $company)
     {
         //
+        return response()->json(
+            [
+                'message' => 'destroy'
+            ]
+        );
     }
 }
