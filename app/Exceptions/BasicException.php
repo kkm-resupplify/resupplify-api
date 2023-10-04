5a3c91a775caa36194ea70c30ca199863b1f7d71
kamil_pula <sorennx@gmail.com>
2023-10-04 22:08:19 +0200

    Added BasicException class

--- app/Exceptions/BasicException.php
+++ app/Exceptions/BasicException.php
@@ -1,3 +1,118 @@
 <?php
 
 namespace App\Exceptions;
+
+use App\Helpers\JsonResponseTrait;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Str;
+
+abstract class BasicException extends \Exception
+{
+    use JsonResponseTrait;
+
+    protected string $errorCode;
+
+    protected int $errorHttpCode;
+
+    protected $errorMsg;
+
+    protected $errorData;
+
+    private string $errorUuid;
+
+    protected string $translationKey;
+
+    protected array $translationParams;
+
+    abstract protected function init();
+
+    public function __construct(
+        $message = '',
+        $code = 0,
+        \Throwable $previous = null
+    ) {
+        parent::__construct($message, $code, $previous);
+
+        $this->init();
+        $this->errorUuid = Str::uuid();
+
+        $this->message = class_basename($this);
+    }
+
+    public function render(): JsonResponse
+    {
+        return $this->responseJson(
+            $this->wrapError(
+                $this->errorMsg,
+                $this->errorHttpCode,
+                $this->errorCode,
+                $this->errorUuid,
+                $this->errorData
+            ),
+            $this->errorHttpCode
+        );
+    }
+
+    /**
+     * Get error code.
+     *
+     * @return string
+     */
+    public function getErrorCode(): string
+    {
+        return $this->errorCode;
+    }
+
+    /**
+     * Get error http code.
+     *
+     * @return int
+     */
+    public function getErrorHttpCode(): int
+    {
+        return $this->errorHttpCode;
+    }
+
+    /**
+     * Get error data field.
+     *
+     * @return mixed
+     */
+    public function getErrorData(): mixed
+    {
+        return $this->errorData;
+    }
+
+    /**
+     * Get uuid of an error.
+     *
+     * @return string
+     */
+    public function getUuid(): string
+    {
+        return $this->errorUuid;
+    }
+
+    /**
+     * Get error message.
+     *
+     * @return string
+     */
+    public function getErrorMessage(): string
+    {
+        return $this->errorMsg;
+    }
+
+    /**
+     * Get translation data.
+     *
+     * @return array
+     */
+    public function getTranslationData(): array
+    {
+        return [
+            'key' => $this->translationKey,
+            'params' => $this->translationParams,
+        ];
+    }
+}
