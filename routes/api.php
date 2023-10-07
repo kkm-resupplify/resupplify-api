ac9616cb59ffaabb531bc678c8f3a89db164f672
potrec <potrec123@wp.pl>
2023-10-07 20:45:38 +0200

    Company endpoint fix

--- routes/api.php
+++ routes/api.php
@@ -3,43 +3,43 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController as AuthController;
-use App\Http\Controllers\Portal\Company\CompanyController as CompanyController;
-use App\Http\Controllers\UserController as UserController;
 use App\Http\Controllers\BackOffice\Country\CountryController as CountryController;
+use App\Http\Controllers\Portal\Company\CompanyController as CompanyController;
 
 Route::post('register', [AuthController::class, 'register'])->name('register');
 Route::post('login', [AuthController::class, 'login']);
-Route::get('test', [CompanyCategoryController::class, 'store']);
-Route::post('country', [CountryController::class, 'create']);
+// Route::get('test', [CompanyCategoryController::class, 'store']);
+
 Route::get('country', [CountryController::class, 'index']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('logout', [AuthController::class, 'logout']);
-    Route::get('companies', [CompanyController::class, 'getCompanies']);
-    Route::get('company/{company_id}', [
-        CompanyController::class,
-        'getCompany',
-    ]);
-    Route::get('company/{company_id}/roles', [
-        CompanyController::class,
-        'getCompanyRoles',
-    ]);
-    Route::get('company/{company_id}/members', [
-        CompanyController::class,
-        'getCompanyMembers',
-    ]);
-    Route::post('company/{company_id}/add-user', [
-        CompanyController::class,
-        'addUserToCompany',
-    ]);
-    Route::post('company/test', [CompanyController::class, 'test']);
-    Route::put('user/editUserDetails', [
-        UserController::class,
-        'editUserDetails',
-    ]);
-    Route::delete('company/deleteUserFromCompany/{user_id}', [
-        CompanyController::class,
-        'deleteUserFromCompany',
-    ]);
     Route::resource('company', CompanyController::class);
+    Route::post('country', [CountryController::class, 'create']);
+    // Route::post('logout', [AuthController::class, 'logout']);
+    // Route::get('companies', [CompanyController::class, 'getCompanies']);
+    // Route::get('company/{company_id}', [
+    //     CompanyController::class,
+    //     'getCompany',
+    // ]);
+    // Route::get('company/{company_id}/roles', [
+    //     CompanyController::class,
+    //     'getCompanyRoles',
+    // ]);
+    // Route::get('company/{company_id}/members', [
+    //     CompanyController::class,
+    //     'getCompanyMembers',
+    // ]);
+    // Route::post('company/{company_id}/add-user', [
+    //     CompanyController::class,
+    //     'addUserToCompany',
+    // ]);
+    // Route::post('company/test', [CompanyController::class, 'test']);
+    // Route::put('user/editUserDetails', [
+    //     UserController::class,
+    //     'editUserDetails',
+    // ]);
+    // Route::delete('company/deleteUserFromCompany/{user_id}', [
+    //     CompanyController::class,
+    //     'deleteUserFromCompany',
+    // ]);
 });
