0e9b827a013743fc74cfefb7cfe2a56df48f8008
Maciej Potręć <73169698+potrec@users.noreply.github.com>
2023-10-16 19:42:04 +0200

    Update UserDetailsService.php

--- app/Services/User/UserDetailsService.php
+++ app/Services/User/UserDetailsService.php
@@ -5,7 +5,9 @@ namespace App\Services\User;
 use App\Models\User\Enums\UserTypeEnum;
 use App\Http\Dto\User\LoginDto;
 use App\Http\Dto\User\PortalRegisterDto;
+use App\Http\Dto\User\UserDetailsDto;
 use App\Models\User\User;
+use App\Models\User\UserDetails;
 use Illuminate\Support\Facades\Hash;
 use App\Exceptions\Auth\FailedLoginException;
 use App\Exceptions\User\UserAlreadyExistsException;
@@ -15,19 +17,21 @@ use Illuminate\Support\Facades\Auth;
 
 class UserDetailsService extends BasicService
 {
-    public function createUserData(PortalRegisterDto $request)
+    public function creatUserData(UserDetailsDto $request)
     {
         $user = Auth::user();
-        // TODO: Write custom rule
         $user = UserDetails::create([
-            'email' => $request->email,
-            'password' => $request->password,
-            'type' => UserTypeEnum::PORTAL(),
+            'first_name' => $request->firstName,
+            'last_name' => $request->lastName,
+            'phone_number' => $request->phoneNumber,
+            'birth_date' => $request->birthDate,
+            'sex' => $request->sex,
+            'user_id' => $user->id,
         ]);
 
         $token = $user->createToken('user_token')->plainTextToken;
 
         return ['user' => $user, 'token' => $token];
     }
-    
+
 }
