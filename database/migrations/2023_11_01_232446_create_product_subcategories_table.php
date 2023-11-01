b9290a5251e5daf6efedef609bbb6a3f3e418031
kamil_pula <sorennx@gmail.com>
2023-11-02 00:30:38 +0100

    Added migrations

---
+++ database/migrations/2023_11_01_232446_create_product_subcategories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_subcategories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table
+                ->foreignId('product_category_id')
+                ->constrained('product_categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_subcategories');
+    }
+};
---
+++ database/migrations/2023_11_01_232853_create_product_product_subcategory_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_product_subcategory', function (
+            Blueprint $table
+        ) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('product_subcategory_id');
+            $table->timestamps();
+
+            $table
+                ->foreign('product_id')
+                ->references('id')
+                ->on('products')
+                ->onDelete('cascade');
+            $table
+                ->foreign('product_subcategory_id')
+                ->references('id')
+                ->on('product_subcategories')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_product_subcategory');
+    }
+};
