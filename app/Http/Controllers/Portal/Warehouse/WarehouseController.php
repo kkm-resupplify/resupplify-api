f7d8f6ccf2673a30fd016b5f7a58fc5da7622060
Maciej Potręć <73169698+potrec@users.noreply.github.com>
2023-11-04 21:15:40 +0100

    Warehouse init

---
+++ app/Http/Controllers/Portal/Warehouse/WarehouseController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Portal\Company;
+
+use App\Http\Controllers\Controller;
+
+
+class WarehouseController extends Controller
+{
+
+}
---
+++ app/Services/Warehouse/WarehouseService.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Services\Company;
+
+use App\Http\Controllers\Controller;
+
+
+
+class WarehouseService extends Controller
+{
+
+}
