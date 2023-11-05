1479a0fff015bcd5fa34d0e2b06040ed8bfac942
Maciej Potręć <73169698+potrec@users.noreply.github.com>
2023-11-05 04:40:55 +0100

    Warehouse create

--- app/Http/Controllers/Portal/Warehouse/WarehouseController.php
+++ app/Http/Controllers/Portal/Warehouse/WarehouseController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Portal\Warehouse;
 
 use App\Http\Controllers\Controller;
+use App\Http\Dto\Warehouse\WarehouseDto;
+use App\Services\Warehouse\WarehouseService;
 
 
 class WarehouseController extends Controller
 {
-    public function createWarehouse()
+    public function createWarehouse(WarehouseService $warehouseService, WarehouseDto $request)
     {
-
+        return $this->ok($warehouseService->createWarehouse($request));
     }
 }
--- app/Services/Warehouse/WarehouseService.php
+++ app/Services/Warehouse/WarehouseService.php
@@ -2,11 +2,32 @@
 
 namespace App\Services\Warehouse;
 
+use App\Exceptions\Company\WrongPermissions;
 use App\Http\Controllers\Controller;
+use App\Http\Dto\Warehouse\WarehouseDto;
+use App\Models\Warehouse\Warehouse;
+use Illuminate\Support\Facades\Auth;
 
 
 
 class WarehouseService extends Controller
 {
+    //create warehouse
+    public function createWarehouse(WarehouseDto $request)
+    {
+        $user = Auth::user();
+        setPermissionsTeamId($user->company->id);
+        if(!$user->can('Owner permissions')) {
+            throw(new WrongPermissions());
+        }
+        $warehouseData = [
+            'name' => $request->name,
+            'description' =>$request->description,
+            'company_id' => $user->company->id,
+        ];
+        $warehouse = new Warehouse($warehouseData);
+        $user->company->warehouses()->save($warehouse);
+        return $warehouse;
+    }
 
 }
