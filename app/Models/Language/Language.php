a93cc4f4b0e749e8ef0059177724b5de530db893
kamil_pula <sorennx@gmail.com>
2023-11-12 01:05:39 +0100

    Added lang to user

--- app/Models/Language/Language.php
+++ app/Models/Language/Language.php
@@ -6,22 +6,25 @@ use App\Models\Product\ProductCategory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+
+use App\Models\User\User;
 
 class Language extends Model
 {
     use HasFactory;
     use SoftDeletes;
 
-    protected $fillable = [
-        'name',
-        'code',
-        'origin_name',
-    ];
+    protected $fillable = ['name', 'code', 'origin_name'];
 
     public function productCategories(): BelongsToMany
     {
         return $this->belongsToMany(ProductCategory::class);
     }
 
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class);
+    }
 }
