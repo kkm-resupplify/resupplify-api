85e36909dd25ebc4458bb2cc5d5ffd38b90b44b9
kamil_pula <sorennx@gmail.com>
2023-11-14 20:43:54 +0100

    Added warehouse product filters by user lang

---
+++ app/Filters/Product/FilterProductName.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Filters\Product;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Builder;
+use Spatie\QueryBuilder\Filters\Filter;
+
+class FilterProductName implements Filter
+{
+    public function __invoke(Builder $query, $value, string $property): Builder
+    {
+        $language_id = Auth::user()->language_id;
+
+        return $query->whereHas('languages', function (Builder $query) use ($value, $language_id) {
+            $query->where('language_product.name', 'like', "%{$value}%")
+                ->where('language_product.language_id', $language_id);
+        });
+    }
+}
