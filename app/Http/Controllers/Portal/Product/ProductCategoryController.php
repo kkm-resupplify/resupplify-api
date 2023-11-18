2b952f35ae93426590aed5a8d7332615ede46fc7
Maciej Potręć <73169698+potrec@users.noreply.github.com>
2023-11-18 20:34:50 +0100

    Clean up code

--- app/Http/Controllers/Portal/Product/ProductCategoryController.php
+++ app/Http/Controllers/Portal/Product/ProductCategoryController.php
@@ -3,13 +3,8 @@
 namespace App\Http\Controllers\Portal\Product;
 
 use App\Http\Controllers\Controller;
-use App\Http\Dto\Product\ProductDto;
-use App\Models\Product\Product;
 use App\Models\Product\ProductCategory;
-use App\Models\Warehouse\Warehouse;
 use App\Services\Product\ProductCategoryService;
-use App\Services\Product\ProductService;
-use Illuminate\Http\Request;
 
 class ProductCategoryController extends Controller
 {
--- app/Http/Controllers/Portal/Product/ProductProductTagController.php
+++ app/Http/Controllers/Portal/Product/ProductProductTagController.php
@@ -3,14 +3,8 @@
 namespace App\Http\Controllers\Portal\Product;
 
 use App\Http\Controllers\Controller;
-use App\Http\Dto\Product\ProductDto;
-use App\Models\Product\Product;
-use App\Models\Product\ProductTag;
-use App\Models\Warehouse\Warehouse;
 use App\Services\Product\ProductProductTagService;
-use App\Services\Product\ProductService;
 use App\Http\Dto\Product\ProductProductTagDto;
-use Illuminate\Http\Request;
 
 class ProductProductTagController extends Controller
 {
