0a28e225f062266a946ce68fd2688738559c26d2
Kamil Pula <kamil.pula.dev@gmail.com>
2023-11-18 12:48:04 +0100

    Updated produt_product_tag_table migration

--- database/migrations/2023_11_01_232449_create_product_product_tag_table.php
+++ database/migrations/2023_11_01_232449_create_product_product_tag_table.php
@@ -4,21 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('product_product_tag', function (Blueprint $table) {
-            $table->id();
+
             $table
-                ->foreign('product_id')
+                ->foreignId('product_id')
                 ->references('id')
                 ->on('products')
                 ->onDelete('cascade');
             $table
-                ->foreign('product_tag_id')
+                ->foreignId('product_tag_id')
                 ->references('id')
                 ->on('product_tags')
                 ->onDelete('cascade');
