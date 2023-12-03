eaca728720ca58217ecabe3b9727453740ee440d
Maciej Potręć <73169698+potrec@users.noreply.github.com>
2023-12-03 21:18:32 +0100

    Update CompanyBalanceService.php

--- app/Services/Company/CompanyBalanceService.php
+++ app/Services/Company/CompanyBalanceService.php
@@ -3,13 +3,18 @@
 namespace App\Services\Company;
 
 
+use App\Http\Dto\Company\CompanyBalanceDto;
 use App\Models\Company\CompanyCategory;
 use App\Services\BasicService;
-
+use Illuminate\Support\Facades\Auth;
+use App\Models\Company\Company;
+use App\Models\Company\CompanyBalanceTransactions;
 class CompanyBalanceService extends BasicService
 {
-    public function createTransaction()
+    public function createTransaction(CompanyBalanceDto $request)
     {
-
+        $user = Auth::user();
+        $company = Company::findOrFail($request->companyId);
+        return $companyBalance = new CompanyBalanceTransactions(['company_id' => $company->companyBalances->company_id,'amount' => $request->amount,'type' => $request->type]);
     }
 }
