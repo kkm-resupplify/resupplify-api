bdcbd6312f60097b674ec78958e61a9d8af3b8c7
Maciej Potręć <73169698+potrec@users.noreply.github.com>
2023-12-15 03:17:40 +0100

    Update ProductOfferDto.php

--- app/Http/Dto/Product/ProductOfferDto.php
+++ app/Http/Dto/Product/ProductOfferDto.php
@@ -5,12 +5,13 @@ namespace App\Http\Dto\Product;
 use App\Http\Dto\BasicDto;
 use Spatie\LaravelData\Attributes\Validation\Max;
 use Spatie\LaravelData\Attributes\Validation\Min;
+use Spatie\LaravelData\Attributes\Validation\Rule;
 
 class ProductOfferDto extends BasicDto
 {
     #[Min(1)]
     public int $productId;
-    #[Min(0.01)]
+    #[Rule(['required', 'regex:/^(0*[1-9][0-9]*(\.[0-9]+)?|0+\.[0-9]*[1-9][0-9]*)$/'])]
     public float $price;
     #[Min(1)]
     public int $offertQuantity;
